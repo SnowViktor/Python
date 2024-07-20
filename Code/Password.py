5f4557684982fc754ff26ceba11f02a92fb9c6ca
Code-Viktor <Snow-Forest@outlook.com>
2024-07-20 13:48:29 +0800

    Upload runway

--- Code/Password.py
+++ Code/Password.py
@@ -45,6 +45,7 @@ def random_generator(length: int) -> str:
     '''
     The password is ASCII charcters and the maximum length of the password is 95 charcters.
     '''
+
     PASSWORD = sample(' !\'"#$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~', 95)
     str_password = ''.join(PASSWORD[:length])
     return str_password
