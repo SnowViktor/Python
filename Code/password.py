40f4642c9a99d26f811baf905ad5d89cced420e3
Code-Viktor <Snow-Forest@outlook.com>
2024-11-06 23:05:02 +0800

    improve caesar_cipher function

--- Code/password.py
+++ Code/password.py
@@ -2,13 +2,16 @@ from random import sample
 
 def caesar_cipher(text: str, offset: int) -> str:
     '''
-    >>> caesar_cipher('Hello, World!', 1)
-    'Ifmmp, Xpsme!'
+    >>> caesar_cipher('Python 3.13.0', 1)
+    'Qzuipo 4.24.1'
     '''
 
     ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
-    OFFSET_MAP = {c: ALPHABET[(i + offset) % 26] for i, c in enumerate(ALPHABET)}
-    OFFSET_MAP.update({c.upper(): ALPHABET[(i + offset) % 26].upper() for i, c in enumerate(ALPHABET)})
+    DIGITS = '0123456789'
+
+    OFFSET_MAP = {a: ALPHABET[(i + offset) % 26] for i, a in enumerate(ALPHABET)}
+    OFFSET_MAP.update({a.upper(): ALPHABET[(i + offset) % 26].upper() for i, a in enumerate(ALPHABET)})
+    OFFSET_MAP.update({d: DIGITS[(int(d) + offset) % 10] for d in DIGITS})
     
     return ''.join(OFFSET_MAP.get(char, char) for char in text)
 
